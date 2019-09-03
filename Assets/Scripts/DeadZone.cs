274ac7d93b5ec806b85a364511170498ffb47cb0
Aju_21 <send2aju@gmail.com>
2019-09-03 17:19:01 +0530

    Added Breakout Game Code and Exe

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadZone : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col)
+	{
+		GM.instance.LoseLife();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 1f;
+	
+	private Vector3 playerPos = new Vector3(0,-8.5f,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -8f, 8f), -8.5f, 0);
+		transform.position = playerPos;
+	}
+}
